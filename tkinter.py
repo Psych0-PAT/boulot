89606efbe5716ffb6d13be1e6b78a06da4099ca0
Pat MATH <pmathevet@gmail.com>
2025-10-10 15:46:10 +0200

    Dimension

---
+++ tkinter.py
@@ -0,0 +1,9 @@
+from tkinter import *  #Importation de la bibliothèque Tkinter
+window =Tk()
+window.mainloop()    #Création de la fenêtre
+window.geometry("800 x 1000") #dimension de la fenêtre
+#window.minsize (width="200",height="200")  #imposer une taille minimale de fenêtre
+
+
+# minuscule ou maj dans les tkinter
+# soucis dans dimensionnement 
