199ad1c6cb3d83e9db52a1b2bd7d1016a6fae550
Pat MATH <pmathevet@gmail.com>
2025-10-15 15:02:14 +0200

    fichier d'essai

---
+++ tkintertuto.py
@@ -0,0 +1,9 @@
+from tkinter import *  #Importation de la bibliothèque Tkinter
+window = Tk ()
+
+window.geometry("800x1000")  #imposer une taille minimale de fenêtre
+
+window.minsize (x=400, y=400)
+
+window.mainloop()    #fermeture de la fenêtre
+
